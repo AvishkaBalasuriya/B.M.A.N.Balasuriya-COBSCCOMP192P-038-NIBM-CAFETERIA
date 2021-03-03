87fcd69e05a39b4b55ea31de0a1127e6d45044b6
AvishkaBalasuriya <avishkabalasuriya980330@gmail.com>
2021-03-03 20:58:50 +0530

    Item model added

--- NIBM-CAFETERIA/Models/ItemModel.swift
+++ NIBM-CAFETERIA/Models/ItemModel.swift
@@ -8,5 +8,19 @@
 import UIKit
 
 class ItemModel: NSObject {
-
+    var foodId:Int
+    var foodName:String
+    var foodDescription:String
+    var foodPrice:Float
+    var foodDiscount:Float
+    var foodPhoto:String
+    
+    init(foodId:Int,foodName:String,foodDescription:String,foodPrice:Float,foodPhoto:String,foodDiscount:Float) {
+        self.foodId=foodId
+        self.foodName=foodName
+        self.foodDescription=foodDescription
+        self.foodPrice=foodPrice
+        self.foodDiscount=foodDiscount
+        self.foodPhoto=foodPhoto
+    }
 }
